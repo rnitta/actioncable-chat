9dc6735b6d7ede2672e9564413767718f8a9a4ce
RN <attinyes@gmail.com>
2017-11-09 03:23:31 +0900

    no message

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -14,19 +14,17 @@ stderr_path "#{rails_root}/log/unicorn_error.log"
 stdout_path "#{rails_root}/log/unicorn.log"
 
 before_fork do |server, worker|
-  if defined?(ActiveRecord::Base)
-    ActiveRecord::Base.connection.disconnect!
-  end
-
+  ENV['BUNDLE_GEMFILE'] = File.expand_path('Gemfile', current_path)
   old_pid = "#{server.config[:pid]}.oldbin"
   if File.exists?(old_pid) && server.pid != old_pid
     begin
-      Process.kill("QUIT", File.read(old_pid).to_i)
+      sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
+      Process.kill(sig, File.read(old_pid).to_i)
     rescue Errno::ENOENT, Errno::ESRCH
     end
   end
-  sleep 1
 end
+
 after_fork do |server, worker|
   Signal.trap 'TERM' do
     puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
